e78f8c0493c759aaf49ab6550b0e8d18b41e0c9c
wryyyyyyyy <no@spam.here>
2021-01-08 17:35:58 +0100

    Changed .travis.yml

--- README.md
+++ README.md
@@ -6,4 +6,3 @@
 | ------------ | ------------ |
 | ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield) | ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg) |
 
-{% include log.tpl %}
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >README.md
 echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
 #wget -oQ- https://travis-ci.com/github/wryyyyyyyy/pages_template|grep log.txt
-pwd && ls -la
-sudo wget https://api.travis-ci.com/v3/job/469819835/log.txt -O docs/_includes/log.tpl
+sudo pwd && sudo ls -la
+sudo wget https://api.travis-ci.com/v3/job/469819835/log.txt -O ./docs/_includes/log.tpl
