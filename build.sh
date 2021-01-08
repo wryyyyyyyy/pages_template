c23e95153c0b0214d30bd0347570b7b3d004914b
wryyyyyyyy <no@spam.here>
2021-01-08 16:11:22 +0100

    Changed includes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >README.md
 echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
 #wget -oQ- https://travis-ci.com/github/wryyyyyyyy/pages_template|grep log.txt
-sudo wget https://api.travis-ci.com/v3/job/469819835/log.txt -O docs/_includes/log.txt
+sudo wget https://api.travis-ci.com/v3/job/469819835/log.txt -O docs/_includes/log.tpl
