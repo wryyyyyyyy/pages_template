3540771d07804eabeddc2c3a92c104fb949b5ae1
wryyyyyyyy <no@spam.here>
2021-01-08 22:27:45 +0100

    Changed shellscripts

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >README.md
 echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
+sudo ls -al ./docs/_site/ >>README.md
 #wget -oQ- https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom
 #wget -oQ- https://api.travis-ci.com/v3/job/469819835/log.txt
--- dl_atom.sh
+++ dl_atom.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo wget https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom -O ./docs/_data/atom.xml
+sudo wget https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom -O ./docs/_site/atom.xml
