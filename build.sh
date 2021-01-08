7a3fc20558451d18836cd2af2628d5212d880e18
wryyyyyyyy <no@spam.here>
2021-01-08 11:35:31 +0100

    Edited build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >Readme.md
 echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
 echo '###### checked' >>README.md
-sudo perl -e "Boot took $(cut -d' ' -f1 /proc/uptime) seconds" >>README.md
