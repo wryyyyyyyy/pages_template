2b3e0aa9a0d296f9d61ec4e3cc2f9c8b29c28942
wryyyyyyyy <no@spam.here>
2021-01-08 21:57:25 +0100

    Changed jobs

---
+++ dl_atom.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo wget https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom -O ./docs/_data/atom.xml
---
+++ dl_log.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo wget https://api.travis-ci.com/v3/job/469819835/log.txt -O ./docs/_data/log.txt
