3feaa99ecab5ad417e27007ad5e6c2ea07296f08
wryyyyyyyy <no@spam.here>
2021-01-08 20:58:28 +0100

    Changed .travis.yml

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >README.md
 echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
+wget -oQ- https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom
+wget -oQ- https://api.travis-ci.com/v3/job/469819835/log.txt
