55356f75e234859ea2e1f17cdba5b90dc20e6027
wryyyyyyyy <no@spam.here>
2021-01-08 09:54:48 +0100

    Changed includes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/runner_one.svg)' >Readme.md
-echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/runner_one.svg)' >>README.md
+echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >Readme.md
+echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
 echo '###### checked' >>README.md
