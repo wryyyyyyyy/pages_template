1762ccd99c1d062d4a72356706229851147c775c
wryyyyyyyy <no@spam.here>
2021-01-09 02:10:55 +0100

    Changed _config.yml

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 sudo bash -c 'echo #### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/pages_template.svg?style=shield)' >README.md
 sudo bash -c 'echo #### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/pages_template.svg)' >>README.md
 sudo bash -c 'ls -al ./docs/_site/' >>README.md
-#wget -oQ- https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom
-#wget -oQ- https://api.travis-ci.com/v3/job/469819835/log.txt
+#wget -qQ- https://api.travis-ci.com/repos/wryyyyyyyy/pages_template/builds.atom
+#wget -qO- https://api.travis-ci.com/v3/job/469819835/log.txt
